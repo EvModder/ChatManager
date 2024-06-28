4137036d6d5a037e593c9ef2cc239a7d4f00d13c
EvModder <evmodder@gmail.com>
2024-06-27 20:56:45 -0500

    Use new getAsString() component method

--- src/net/evmodder/ChatManager/JunkUtils.java
+++ src/net/evmodder/ChatManager/JunkUtils.java
@@ -19,10 +19,10 @@ public class JunkUtils{
 	final static RefClass craftItemStackClazz = ReflectionUtils.getRefClass("{cb}.inventory.CraftItemStack");
 	final static RefMethod asNMSCopyMethod = craftItemStackClazz.getMethod("asNMSCopy", ItemStack.class);
 
-	// NMS Method to serialize a net.minecraft.server.vX_X.ItemStack to a valid JSON string
-	final static RefClass nmsItemStackClazz = ReflectionUtils.getRefClass("{nms}.ItemStack", "{nm}.world.item.ItemStack");
-	final static RefClass nbtTagCompoundClazz = ReflectionUtils.getRefClass("{nms}.NBTTagCompound", "{nm}.nbt.NBTTagCompound");
-	final static RefMethod saveNmsItemStackMethod = nmsItemStackClazz.findMethod(/*isStatic=*/false, nbtTagCompoundClazz, nbtTagCompoundClazz);
+//	// NMS Method to serialize a net.minecraft.server.vX_X.ItemStack to a valid JSON string
+//	final static RefClass nmsItemStackClazz = ReflectionUtils.getRefClass("{nms}.ItemStack", "{nm}.world.item.ItemStack");
+//	final static RefClass nbtTagCompoundClazz = ReflectionUtils.getRefClass("{nms}.NBTTagCompound", "{nm}.nbt.NBTTagCompound");
+//	final static RefMethod saveNmsItemStackMethod = nmsItemStackClazz.findMethod(/*isStatic=*/false, nbtTagCompoundClazz, nbtTagCompoundClazz);
 
 	// https://www.spigotmc.org/threads/tut-item-tooltips-with-the-chatcomponent-api.65964/
 	/**
@@ -33,20 +33,29 @@ public class JunkUtils{
 	 * @return the JSON string representation of the item
 	 */
 	public final static String convertItemStackToJson(ItemStack item, int JSON_LIMIT){
-		Object nmsNbtTagCompoundObj = nbtTagCompoundClazz.getConstructor().create();
-		Object nmsItemStackObj = asNMSCopyMethod.of(null).call(item);
-		Object itemAsJsonObject = saveNmsItemStackMethod.of(nmsItemStackObj).call(nmsNbtTagCompoundObj);
-		String jsonString = itemAsJsonObject.toString();
-		if(jsonString.length() > JSON_LIMIT){
-			item = new ItemStack(item.getType(), item.getAmount());//TODO: Reduce item json data in a less destructive way-
+		if(item.hasItemMeta()){
+			String jsonString = "{id:\""+item.getType().getKey().getKey()+"\",count:"+item.getAmount()+",components:"+item.getItemMeta().getAsString()+"}";
+			if(jsonString.length() <= JSON_LIMIT) return jsonString;
+			//TODO: Reduce item json data in a less destructive way
 			//reduceItemData() -> clear book pages, clear hidden NBT, call recursively for containers
-			nmsNbtTagCompoundObj = nbtTagCompoundClazz.getConstructor().create();
-			nmsItemStackObj = asNMSCopyMethod.of(null).call(item);
-			itemAsJsonObject = saveNmsItemStackMethod.of(nmsItemStackObj).call(nmsNbtTagCompoundObj);
-			jsonString = itemAsJsonObject.toString();
 		}
-		return itemAsJsonObject.toString();
+		return "{id:\""+item.getType().getKey().getKey()+"\",count:"+item.getAmount()+"}";
 	}
+//	public final static String convertItemStackToJson(ItemStack item, int JSON_LIMIT){
+//		Object nmsNbtTagCompoundObj = nbtTagCompoundClazz.getConstructor().create();
+//		Object nmsItemStackObj = asNMSCopyMethod.of(null).call(item);
+//		Object itemAsJsonObject = saveNmsItemStackMethod.of(nmsItemStackObj).call(nmsNbtTagCompoundObj);
+//		String jsonString = itemAsJsonObject.toString();
+//		if(jsonString.length() > JSON_LIMIT){
+//			item = new ItemStack(item.getType(), item.getAmount());//TODO: Reduce item json data in a less destructive way-
+//			//reduceItemData() -> clear book pages, clear hidden NBT, call recursively for containers
+//			nmsNbtTagCompoundObj = nbtTagCompoundClazz.getConstructor().create();
+//			nmsItemStackObj = asNMSCopyMethod.of(null).call(item);
+//			itemAsJsonObject = saveNmsItemStackMethod.of(nmsItemStackObj).call(nmsNbtTagCompoundObj);
+//			jsonString = itemAsJsonObject.toString();
+//		}
+//		return itemAsJsonObject.toString();
+//	}
 
 	// Different from FileIO only in that it doesn't trim trailing/leading line whitespace
 	public static String loadResource(Object pl, String filename/*, boolean keepComments*/){
